cec76956b46271cf5b63eb8b6e0fd6a8bcc2fa46
Dio Aditya Putra Wardhono <55073908+dioapw@users.noreply.github.com>
2021-03-10 01:07:32 +0700

    Add files via upload

---
+++ 4A - A. Watermelon.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void divide (int w)
+{
+  if (w % 2 == 0 && w > 2)
+    {
+      std::cout << "YES";
+    }
+  else
+    {
+      std::cout << "NO";
+    }
+}
+
+int main ()
+{
+  int x;
+  std::cin >> x;
+  divide (x);
+  return 0;
+}
